9540486fc478b4b59b8bb02418aa936e121d2bb7
MargVander <margauxvanderbeken@gmail.com>
2020-02-21 12:26:46 +0100

    new try with update-electron-app

--- public/electron.js
+++ public/electron.js
@@ -5,17 +5,17 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
-const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
+
+require('update-electron-app')({
+  updateInterval: '5 minutes',
+})
 
 function createWindow() {
   mainWindow = new BrowserWindow({width: 900, height: 680});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
-  mainWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
-  });
 }
 
 app.on('ready', createWindow);
@@ -31,9 +31,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-  });
-  autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-  });
